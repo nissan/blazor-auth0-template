30b3c746f45d29c46743de29885a2046efe31093
Nissan Dookeran <nissan.dookeran@jjswaste.com.au>
2020-10-08 14:46:34 +1000

    Add or modify components to support Auth0 login authentication

--- Demo.Blazor.Auth0.SS.WebApp/Startup.cs
+++ Demo.Blazor.Auth0.SS.WebApp/Startup.cs
@@ -1,14 +1,17 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Demo.Blazor.Auth0.SS.WebApp.Data;
 
 namespace Demo.Blazor.Auth0.SS.WebApp
@@ -28,6 +31,73 @@ namespace Demo.Blazor.Auth0.SS.WebApp
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            // Add authentication services
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOpenIdConnect("Auth0", options =>
+            {
+                // Set the authority to your Auth0 domain
+                options.Authority = $"https://{Configuration["Auth0:Domain"]}";
+
+                // Configure the Auth0 Client ID and Client Secret
+                options.ClientId = Configuration["Auth0:ClientId"];
+                options.ClientSecret = Configuration["Auth0:ClientSecret"];
+
+                // Set response type to code
+                options.ResponseType = "code";
+
+                // Configure the scope
+                options.Scope.Clear();
+                options.Scope.Add("openid");
+
+                // Set the callback path, so Auth0 will call back to http://localhost:3000/callback
+                // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
+                options.CallbackPath = new PathString("/callback");
+
+                // Configure the Claims Issuer to be Auth0
+                options.ClaimsIssuer = "Auth0";
+
+                options.Events = new OpenIdConnectEvents
+                {
+                    // handle the logout redirection
+                    OnRedirectToIdentityProviderForSignOut = (context) =>
+                      {
+                          var logoutUri = $"https://{Configuration["Auth0:Domain"]}/v2/logout?client_id={Configuration["Auth0:ClientId"]}";
+
+                          var postLogoutUri = context.Properties.RedirectUri;
+                          if (!string.IsNullOrEmpty(postLogoutUri))
+                          {
+                              if (postLogoutUri.StartsWith("/"))
+                              {
+                                  // transform to absolute
+                                  var request = context.Request;
+                                  postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                              }
+                              logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
+                          }
+
+                          context.Response.Redirect(logoutUri);
+                          context.HandleResponse();
+
+                          return Task.CompletedTask;
+                      }
+                };
+            });
+
+            services.AddHttpContextAccessor();
+
             services.AddSingleton<WeatherForecastService>();
         }
 
@@ -49,6 +119,10 @@ namespace Demo.Blazor.Auth0.SS.WebApp
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
